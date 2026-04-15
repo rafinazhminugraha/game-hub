915c0887f54444c86f905a13a321c7c5e9e80935
Rafi Nazhmi Nugraha <rafinazhminugraha@gmail.com>
2026-04-15 11:30:19 +0700

    rebuild the theme toogle

--- src/lib/useTheme.ts
+++ src/lib/useTheme.ts
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
 
 export function useTheme() {
+  // it means to get boolean data about is there a .dark class on html or not
+  // for restoreing last prefrence of the suer
   const [isDark, setIsDark] = useState(() => {
     return localStorage.getItem("theme") === "dark";
   });
 
   useEffect(() => {
-    const root = document.documentElement; // that's <html>
+    const root = document.documentElement;
+
     if (isDark) {
       root.classList.add("dark");
     } else {
       root.classList.remove("dark");
     }
+
     localStorage.setItem("theme", isDark ? "dark" : "light");
   }, [isDark]);
 
+  // its for other component(mainly the toogle) can toogle the theme
   const toggle = () => setIsDark((prev) => !prev);
 
   return { isDark, toggle };
