95c94f6ef63b0b8d3a5add92f8520f014214cb37
Rafi Nazhmi Nugraha <rafinazhminugraha@gmail.com>
2026-04-28 14:48:37 +0700

    migrate useGames hook from native fetching to tanstack query

--- src/components/game/GameGrid.tsx
+++ src/components/game/GameGrid.tsx
@@ -12,16 +12,16 @@ interface Props {
 }
 
 export default function GameGrid({ query }: Props) {
-  const { games, error, loading } = useGames(query);
+  const { data = [], isPending, error } = useGames(query);
 
-  if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-      {loading
+      {isPending
         ? Array.from({ length: 10 }).map((_, index) => (
             <GameCardSkeleton key={index} />
           ))
-        : games.map((item) => <GameCard key={item.id} game={item} />)}
+        : data.map((item) => <GameCard key={item.id} game={item} />)}
     </section>
   );
 }
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { AxiosError, CanceledError } from "axios";
 import { type Platform } from "./usePlatforms";
 import { type Genre } from "./useGenres";
+import { useQuery } from "@tanstack/react-query";
 
 // 3. Look for the API Doc to see the structer (use postman and hit the endpoint)
 // 4. make the interface to get ony needed data (Destructering)
@@ -16,11 +15,6 @@ export interface Game {
   genres: Genre[];
 }
 
-interface FetchResponse {
-  count: number;
-  results: Game[];
-}
-
 export interface GameQuery {
   genre?: string;
   platform?: string;
@@ -30,48 +24,71 @@ export interface GameQuery {
 
 // 5. Make the Costum hooks so we can use it anywhere on our app
 const useGames = ({ genre, platform, sort, search }: GameQuery) => {
-  //usually there's data, error and loading
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(true);
+  //---------------------------------------------------------------------
+  //      USING TANSTACK QUERY (EASY FETCHING CACHING AND EVERYTHING)
+  //---------------------------------------------------------------------
+  const {isPending, error, data} = useQuery<Game[]>({
+    queryKey: ["games", genre, platform, sort, search],
+    queryFn: async ({ signal }) => {
+      const response = await apiClient.get("/games", {
+        signal,
+        params: {
+          genres: genre || undefined,
+          platforms: platform || undefined,
+          ordering: sort || undefined,
+          search: search || undefined,
+        },
+      });
 
-  useEffect(() => {
-    const controller = new AbortController();
-    // if (!genre && !platform && !sort && !search) return;
-    const fetchGame = async () => {
-      try {
-        setLoading(true);
-        const res = await apiClient.get<FetchResponse>("/games", {
-          signal: controller.signal,
-          params: {
-            genres: genre || undefined,
-            platforms: platform || undefined,
-            ordering: sort || undefined,
-            search: search || undefined,
-          },
-        });
-        setGames(res.data.results);
-        setLoading(false)
-      } catch (e) {
-        if (e instanceof CanceledError) {
-          return;
-        }
-        if (e instanceof AxiosError) {
-          setError(e.message);
-        }
-      } 
-      // always bug when using finally in strict mode
-      // Use this when production!!
-      // finally {
-      //   setLoading(false)
-      // }
-    };
-    fetchGame();
-    
-    return () => controller.abort();
-  }, [genre, platform, sort, search]);
+      return response.data.results;
+    }
+  });
 
-  return { games, error, loading };
+  return {isPending, error, data};
+
+
+  //---------------------------------------------------------------------
+  //                  USING NATIVE FETCH FROM JS AND REACT
+  //---------------------------------------------------------------------
+  // //usually there's data, error and loading
+  // const [games, setGames] = useState<Game[]>([]);
+  // const [error, setError] = useState("");
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   const controller = new AbortController();
+  //   // if (!genre && !platform && !sort && !search) return;
+  //   const fetchGame = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const res = await apiClient.get<FetchResponse>("/games", {
+  //         signal: controller.signal,
+  //         params: {
+  //           genres: genre || undefined,
+  //           platforms: platform || undefined,
+  //           ordering: sort || undefined,
+  //           search: search || undefined,
+  //         },
+  //       });
+  //       setGames(res.data.results);
+  //       setLoading(false)
+  //     } catch (e) {
+  //       if (e instanceof CanceledError) {
+  //         return;
+  //       }
+  //       if (e instanceof AxiosError) {
+  //         setError(e.message);
+  //       }
+  //     }
+  //     // always bug when using finally in strict mode
+  //     // Use this when production!!
+  //     // finally {
+  //     //   setLoading(false)
+  //     // }
+  //   };
+  //   fetchGame();
+  //   return () => controller.abort();
+  // }, [genre, platform, sort, search]);
+  // return { games, error, loading };
 };
 
 export default useGames;
